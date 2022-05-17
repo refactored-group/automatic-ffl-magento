2e099023d06c093b58219f5508c8c27f7d85c650
Andresa Martins <amartins@razoyo.com>
2022-05-17 12:41:03 -0300

    FRM-26 — Redirect to multi-shipping if available

--- core/Observer/Checkout/Index.php
+++ core/Observer/Checkout/Index.php
@@ -63,23 +63,20 @@ class Index implements ObserverInterface
     {
         if ($this->helper->isEnabled() && $this->helper->hasFflItem() && !$this->helper->isFflCart()) {
             $message  = 'Your cart has items that need to be shipped to a Dealer. ';
-            if ($this->helper->isMultishippingCheckoutAvailable()) {
-                $message .= 'You can not perform a regular checkout with a mixed cart. ';
-                $message .= 'Please, checkout using the "Check Out with Multiple Addresses" option. ';
-            } else {
+            if (!$this->helper->isMultishippingCheckoutAvailable()) {
                 // @TODO: This message seems a little confusing, we need to work on a better one
                 $message .= 'You can not checkout with a mixed cart. ';
                 $message .= 'Please remove all items from your cart that need to be shipped to a Dealer or the items that do not.';
+                $this->messageManager->addErrorMessage(__($message));
             }
 
-            $this->messageManager->addErrorMessage(__($message));
             if ($observer->getEvent()->getName() == 'sales_order_place_before') {
                 echo $this->url->getUrl('checkout/cart/index');
                 exit;
             } else if ($observer->getEvent()->getName() !== 'controller_action_predispatch_checkout_cart_index') {
                 $observer->getControllerAction()
                     ->getResponse()
-                    ->setRedirect($this->url->getUrl('checkout/cart/index'));
+                    ->setRedirect($this->url->getUrl('multishipping/checkout'));
             }
         }
     }
