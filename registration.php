d6c6502788d38f8a5edf5178d40b316aa37547fc
Giancarlo Byrne <gbyrne@razoyo.com>
2023-03-30 10:05:48 -0500

    FRM-135 - Composer updates to meet Magento Requirements

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Refactored Group (https://www.refactored.group)
+ * @copyright Copyright © 2022. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'RefactoredGroup_AutoFflMagento',
+    __DIR__
+);
