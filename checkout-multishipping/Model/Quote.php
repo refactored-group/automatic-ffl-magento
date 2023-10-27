422c2268f9c9f093330e6c4b960210b57b6143d4
Tomas Cabagay <tomas.cabagay@gmail.com>
2023-10-27 18:03:09 +0000

    FFL-266 - Magento: Multiaddress Checkout Not Displaying Quantities Of Products Correctly

---
+++ checkout-multishipping/Model/Quote.php
@@ -0,0 +1,246 @@
+<?php
+/**
+ * Copyright © Refactored Group (https://www.refactored.group)
+ * @copyright Copyright © 2022. All rights reserved.
+ */
+namespace RefactoredGroup\AutoFflCheckoutMultiShipping\Model;
+
+use Magento\Directory\Model\AllowedCountries;
+use Magento\Framework\Api\AttributeValueFactory;
+use Magento\Framework\Api\ExtensionAttribute\JoinProcessorInterface;
+use Magento\Sales\Model\Status;
+
+class Quote extends \Magento\Quote\Model\Quote
+{
+    private const FFL_CHECKOUT_BUTTON_KEY = 'ffl_checkout_button_clicked';
+
+    /**
+     * Quote shipping addresses items cache
+     *
+     * @var array
+     */
+    protected $shippingAddressesItems;
+
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    private $customerSession;
+
+    /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
+     * @param AttributeValueFactory $customAttributeFactory
+     * @param QuoteValidator $quoteValidator
+     * @param \Magento\Catalog\Helper\Product $catalogProduct
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $config
+     * @param Quote\AddressFactory $quoteAddressFactory
+     * @param \Magento\Customer\Model\CustomerFactory $customerFactory
+     * @param \Magento\Customer\Api\GroupRepositoryInterface $groupRepository
+     * @param \Magento\Quote\Model\ResourceModel\Quote\Item\CollectionFactory $quoteItemCollectionFactory
+     * @param Quote\ItemFactory $quoteItemFactory
+     * @param \Magento\Framework\Message\Factory $messageFactory
+     * @param Status\ListFactory $statusListFactory
+     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
+     * @param Quote\PaymentFactory $quotePaymentFactory
+     * @param \Magento\Quote\Model\ResourceModel\Quote\Payment\CollectionFactory $quotePaymentCollectionFactory
+     * @param \Magento\Framework\DataObject\Copy $objectCopyService
+     * @param \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry
+     * @param Quote\Item\Processor $itemProcessor
+     * @param \Magento\Framework\DataObject\Factory $objectFactory
+     * @param \Magento\Customer\Api\AddressRepositoryInterface $addressRepository
+     * @param \Magento\Framework\Api\SearchCriteriaBuilder $criteriaBuilder
+     * @param \Magento\Framework\Api\FilterBuilder $filterBuilder
+     * @param \Magento\Customer\Api\Data\AddressInterfaceFactory $addressDataFactory
+     * @param \Magento\Customer\Api\Data\CustomerInterfaceFactory $customerDataFactory
+     * @param \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository
+     * @param \Magento\Framework\Api\DataObjectHelper $dataObjectHelper
+     * @param \Magento\Framework\Api\ExtensibleDataObjectConverter $extensibleDataObjectConverter
+     * @param Cart\CurrencyFactory $currencyFactory
+     * @param JoinProcessorInterface $extensionAttributesJoinProcessor
+     * @param Quote\TotalsCollector $totalsCollector
+     * @param Quote\TotalsReader $totalsReader
+     * @param ShippingFactory $shippingFactory
+     * @param ShippingAssignmentFactory $shippingAssignmentFactory
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
+     * @param array $data
+     * @param \Magento\Sales\Model\OrderIncrementIdChecker|null $orderIncrementIdChecker
+     * @param AllowedCountries|null $allowedCountriesReader
+     * @param \Magento\Framework\App\Request\Http $request
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        AttributeValueFactory $customAttributeFactory,
+        \Magento\Quote\Model\QuoteValidator $quoteValidator,
+        \Magento\Catalog\Helper\Product $catalogProduct,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config,
+        \Magento\Quote\Model\Quote\AddressFactory $quoteAddressFactory,
+        \Magento\Customer\Model\CustomerFactory $customerFactory,
+        \Magento\Customer\Api\GroupRepositoryInterface $groupRepository,
+        \Magento\Quote\Model\ResourceModel\Quote\Item\CollectionFactory $quoteItemCollectionFactory,
+        \Magento\Quote\Model\Quote\ItemFactory $quoteItemFactory,
+        \Magento\Framework\Message\Factory $messageFactory,
+        \Magento\Sales\Model\Status\ListFactory $statusListFactory,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\Quote\Model\Quote\PaymentFactory $quotePaymentFactory,
+        \Magento\Quote\Model\ResourceModel\Quote\Payment\CollectionFactory $quotePaymentCollectionFactory,
+        \Magento\Framework\DataObject\Copy $objectCopyService,
+        \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry,
+        \Magento\Quote\Model\Quote\Item\Processor $itemProcessor,
+        \Magento\Framework\DataObject\Factory $objectFactory,
+        \Magento\Customer\Api\AddressRepositoryInterface $addressRepository,
+        \Magento\Framework\Api\SearchCriteriaBuilder $criteriaBuilder,
+        \Magento\Framework\Api\FilterBuilder $filterBuilder,
+        \Magento\Customer\Api\Data\AddressInterfaceFactory $addressDataFactory,
+        \Magento\Customer\Api\Data\CustomerInterfaceFactory $customerDataFactory,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper,
+        \Magento\Framework\Api\ExtensibleDataObjectConverter $extensibleDataObjectConverter,
+        \Magento\Quote\Model\Cart\CurrencyFactory $currencyFactory,
+        JoinProcessorInterface $extensionAttributesJoinProcessor,
+        \Magento\Quote\Model\Quote\TotalsCollector $totalsCollector,
+        \Magento\Quote\Model\Quote\TotalsReader $totalsReader,
+        \Magento\Quote\Model\ShippingFactory $shippingFactory,
+        \Magento\Quote\Model\ShippingAssignmentFactory $shippingAssignmentFactory,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = [],
+        \Magento\Sales\Model\OrderIncrementIdChecker $orderIncrementIdChecker = null,
+        AllowedCountries $allowedCountriesReader = null
+    ) {
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $quoteValidator,
+            $catalogProduct,
+            $scopeConfig,
+            $storeManager,
+            $config,
+            $quoteAddressFactory,
+            $customerFactory,
+            $groupRepository,
+            $quoteItemCollectionFactory,
+            $quoteItemFactory,
+            $messageFactory,
+            $statusListFactory,
+            $productRepository,
+            $quotePaymentFactory,
+            $quotePaymentCollectionFactory,
+            $objectCopyService,
+            $stockRegistry,
+            $itemProcessor,
+            $objectFactory,
+            $addressRepository,
+            $criteriaBuilder,
+            $filterBuilder,
+            $addressDataFactory,
+            $customerDataFactory,
+            $customerRepository,
+            $dataObjectHelper,
+            $extensibleDataObjectConverter,
+            $currencyFactory,
+            $extensionAttributesJoinProcessor,
+            $totalsCollector,
+            $totalsReader,
+            $shippingFactory,
+            $shippingAssignmentFactory,
+            $resource,
+            $resourceCollection,
+            $data,
+            $orderIncrementIdChecker,
+            $allowedCountriesReader
+        );
+
+        $this->customerSession = $customerSession;
+    }
+
+    /**
+     * Get quote items assigned to different quote addresses populated per item qty.
+     *
+     * @return array
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    public function getShippingAddressesItems()
+    {
+        if ($this->shippingAddressesItems !== null) {
+            return $this->shippingAddressesItems;
+        }
+        $items = [];
+        $addresses = $this->getAllAddresses();
+        foreach ($addresses as $address) {
+            foreach ($address->getAllItems() as $item) {
+                if ($item->getParentItemId()) {
+                    continue;
+                }
+                if ($item->getProduct()->getIsVirtual()) {
+                    $items[] = $item;
+                    continue;
+                }
+                if ($this->customerSession->getData(self::FFL_CHECKOUT_BUTTON_KEY) &&
+                    $this->customerSession->getData(self::FFL_CHECKOUT_BUTTON_KEY) === 'proceed_to_checkout') {
+                    $item->setCustomerAddressId($address->getCustomerAddressId())->save();
+                    $items[] = $item;
+                } else {
+                    if ($item->getQty() > 1) {
+                        //DB table `quote_item` qty value can not be set to 1, if having more than 1 child references
+                        //in table `quote_address_item`.
+                        if ($item->getItemId() !== null
+                            && count($this->getQuoteShippingAddressItemsByQuoteItemId($item->getItemId())) > 1) {
+                            continue;
+                        }
+                        for ($itemIndex = 0, $itemQty = $item->getQty(); $itemIndex < $itemQty; $itemIndex++) {
+                            if ($itemIndex === 0) {
+                                $addressItem = $item;
+                            } else {
+                                $addressItem = clone $item;
+                            }
+                            $addressItem->setQty(1)->setCustomerAddressId($address->getCustomerAddressId())->save();
+                            $items[] = $addressItem;
+                        }
+                    } else {
+                        $item->setCustomerAddressId($address->getCustomerAddressId());
+                        $items[] = $item;
+                    }
+                }
+            }
+        }
+        $this->shippingAddressesItems = $items;
+        return $items;
+    }
+
+    /**
+     * Returns quote address items
+     *
+     * @param int $itemId
+     * @return array
+     */
+    private function getQuoteShippingAddressItemsByQuoteItemId(int $itemId): array
+    {
+        $addressItems = [];
+        if ($this->isMultipleShippingAddresses()) {
+            $addresses = $this->getAllShippingAddresses();
+            foreach ($addresses as $address) {
+                foreach ($address->getAllItems() as $item) {
+                    if ($item->getParentItemId() || $item->getProduct()->getIsVirtual()) {
+                        continue;
+                    }
+                    if ((int)$item->getQuoteItemId() === $itemId) {
+                        $addressItems[] = $item;
+                    }
+                }
+            }
+        }
+
+        return $addressItems;
+    }
+}
---
+++ checkout-multishipping/Plugin/Checkout/Controller/Cart/Index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright © Refactored Group (https://www.refactored.group)
+ * @copyright Copyright © 2022. All rights reserved.
+ */
+namespace RefactoredGroup\AutoFflCheckoutMultiShipping\Plugin\Checkout\Controller\Cart;
+
+use Closure;
+use Magento\Checkout\Controller\Cart\Index as ParentController;
+use Magento\Customer\Model\Session;
+
+class Index
+{
+    private const FFL_CHECKOUT_BUTTON_KEY = 'ffl_checkout_button_clicked';
+
+    /**
+     * @var Session
+     */
+    private $customerSession;
+
+    /**
+     * @param Session $customerSession
+     */
+    public function __construct(
+        Session $customerSession,
+    ) {
+        $this->customerSession = $customerSession;
+    }
+
+    /**
+     * Redirects customers without a session to the multi-shipping cart
+     * if the feature is enabled and has a FFL item in the quote
+     *
+     * @param ParentController $subject
+     * @param Closure $proceed
+     * @return ResultInterface
+     */
+    public function aroundExecute(ParentController $subject, Closure $proceed)
+    {
+        // Clear the customerSession in Shopping Cart page.
+        $this->customerSession->unsetData(self::FFL_CHECKOUT_BUTTON_KEY);
+
+        return $proceed();
+    }
+}
