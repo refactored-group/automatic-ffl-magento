bf6cadf582a2837b3728c2317a5763aa11538a11
Tomas Cabagay <tomas.cabagay@gmail.com>
2023-11-14 16:21:06 +0000

    FFL-270 - Convert Composer Patch To Magento Module Patch

---
+++ amasty-xnotif/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    config: {
+        mixins: {
+            'mage/validation': {
+                'Amasty_Xnotif/js/validation-mixin': false,
+                'RefactoredGroup_Amasty_Xnotif/js/validation-mixin': true
+            }
+        }
+    }
+};
